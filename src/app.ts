e0e4e817218cbf6f7b668b43bc69d5330e4c9348
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-06-29 22:05:25 +0530

    update: morgan added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import compression from 'compression';
+import morgan from 'morgan';
 // DATABASE
 import connectDB from './config/db';
 connectDB();
@@ -8,6 +9,8 @@ connectDB();
 import { notFound, errorHandler } from './middlewares/errorMiddleware';
 // Routes
 import studentRoutes from './routes/studentRoutes';
+// Logger
+import logger from './utils/logger';
 
 // APP
 const app = express();
@@ -16,6 +19,25 @@ const app = express();
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// MORGAN
+const morganFormat = ':method :url :status :response-time ms';
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(' ')[0],
+          url: message.split(' ')[1],
+          status: message.split(' ')[2],
+          responseTime: message.split(' ')[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 // Routes
 app.use('/api/v1/students', studentRoutes);
